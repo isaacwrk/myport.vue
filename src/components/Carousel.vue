fd59cdcc5ed92f3692ca0dab8e7656a20df273f2
Isaac Souza <isaacsouzawrk@gmail.com>
2021-10-04 16:14:06 -0300

    fixing carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,15 +1,18 @@
 <template>
-  <v-carousel show-arrows-on-hover height="auto">
+  <v-carousel show-arrows-on-hover height="100%" :style="{'height': this.carouselHeight}">
     <v-carousel-item
       v-for="(item,i) in items"
       :key="i"
       :src="item.src"
+      style="height: auto" 
+      id="carousel"
     ></v-carousel-item>
   </v-carousel>
 </template>
 
 <script>
   export default {
+    carouselHeight: 0,
     data () {
       return {
         items: [
@@ -24,13 +27,19 @@
           },
           {
             src: 'https://github.com/isaacwrk/js-exercises/blob/master/ex/js.png?raw=true',
-          },
-          {
-            src:'https://github.com/isaacwrk/js-exercises/blob/master/ex/ct1.png?raw=true'
           }
         ],
       }
     },
+    methods:{
+      getCarouselHeight () {
+      var item = document.getElementsByClassName('v-image__image--cover')
+      this.carouselHeight = item[0].clientHeight + 'px'
+      }
+    },
+    mounted () {
+      this.getCarouselHeight()
+    }
   }
 </script>
 
